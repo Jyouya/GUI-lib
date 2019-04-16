ba6952cafc14c4070cd80439f1be2f2d0ea56ea5
Jyouya <48452084+Jyouya@users.noreply.github.com>
2019-04-16 01:14:33 -0400

    Update PassiveText.lua

--- GUI/PassiveText.lua
+++ GUI/PassiveText.lua
@@ -52,6 +52,16 @@ _meta.PassiveText.__methods['update'] = function(pt)
 	windower.text.set_text(tostring(pt), (pt._track._text or '%s'):format(table.foreach(pt._track._var, function(_,x) if type(x) == 'string' then return _G[x] or 'None' elseif type(x) == 'function' then return x() or 'None' end end)))
 end
 
+_meta._PassiveText.__methods['Recolor'] = function(pt, alpha, red, green, blue)
+	pt._track._color = {alpha, red, green, blue}
+	windower.text.set_color(tostring(pt), alpha, red, green, blue)
+end
+
+_meta._PassiveText.__methods['Restroke'] = function(pt, alpha, red, green, blue)
+	pt._track._stroke_color = {alpha, red, green, blue}
+	windower.text.set_stroke_color(tostring(pt), alpha, red, green, blue)
+end
+
 _meta.PassiveText.__index = function(pt, k)
     if type(k) == 'string' then
 		
